06cc920b1a72a5d5b42f94261299d5dca0ca4962
AndyShaw <zhexiao@163.com>
2013-03-19 15:31:19 -0400

    update

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*define("APP_NAME","Web");
+define("APP_PATH", "./Web/");
+*/
+define('APP_DEBUG', true);
+
+define('BUILD_DIR_SECURE',true);
+define('DIR_SECURE_FILENAME', 'default.html');
+define('DIR_SECURE_CONTENT', 'deney Access!');
+
+/*if( file_exists('./Web/Runtime/~runtime.php') ){
+	require './Web/Runtime/~runtime.php';
+}else{*/
+	require './ThinkPHP/ThinkPHP.php';
+/*}*/
