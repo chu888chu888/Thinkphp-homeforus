f401e9f516483a9c15d56b85d92a3067f7c59708
AndyShaw <zhexiao@163.com>
2013-03-19 15:40:20 -0400

    update

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ define('BUILD_DIR_SECURE',true);
 define('DIR_SECURE_FILENAME', 'default.html');
 define('DIR_SECURE_CONTENT', 'deney Access!');
 
+
 /*if( file_exists('./Web/Runtime/~runtime.php') ){
 	require './Web/Runtime/~runtime.php';
 }else{*/
